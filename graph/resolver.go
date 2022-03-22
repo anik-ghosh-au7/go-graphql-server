f912ee29d1973a3bf1a066f1ece47750497fd8b2
Anik Ghosh <tech.anikghosh@gmail.com>
2022-03-22 21:56:07 +0530

    init

---
+++ graph/resolver.go
@@ -0,0 +1,12 @@
+package graph
+
+//go:generate go run github.com/99designs/gqlgen generate
+import "github.com/anik-ghosh-au7/go-graphql-server/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	todos []*model.Todo
+}
---
+++ graph/schema.resolvers.go
@@ -0,0 +1,52 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/anik-ghosh-au7/go-graphql-server/graph/generated"
+	"github.com/anik-ghosh-au7/go-graphql-server/graph/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	todo := &model.Todo{
+		Text:   input.Text,
+		ID:     fmt.Sprintf("T%d", generateID()),
+		UserID: input.UserID,
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	return r.todos, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func generateID() *big.Int {
+	nBig, err := rand.Int(rand.Reader, big.NewInt(256))
+	if err != nil {
+		log.Println(err)
+	}
+	return nBig
+}
